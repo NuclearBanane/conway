472bc827581abc2d09d4e8258e81feff14019d0c
Jeffrey Douangpaseuth <jdouangpaseuth@hotmail.ca>
2017-12-03 10:43:46 -0500

    emain barriers

--- src/emain.c
+++ src/emain.c
@@ -14,32 +14,35 @@ volatile uint32_t *shared_state;
 char* swap SECTION(".text_bank2"); // 0x4000
 /* Swap Structure:
  *  -> 0x4000+n_res-1:
- *      General-purpose swap chars
+ *    General-purpose swap chars
  *  -> 0x4000+n_res+n-1:
- *      States for all game pnts managed by this core
+ *    States for all game pnts managed by this core
  *  -> 0x4000+n_res+n+8n:
- *      Adjacent states for all managed cores
- *      Written in by adjacency broadcasts
+ *    Adjacent states for all managed cores
+ *    Written in by adjacency broadcasts
  */
 
 int main(void) {
 	uint32_t iterations = 0;
 	uint32_t iof = 0; // Sticky Integer Overflow Flag
-	unsigned n = ((uint8_t) swap[0]) / 4; // # pnts managed by this core
+	unsigned n = ((uint8_t) swap[0]) / 4; // # core-managed pnts
 
 	unsigned e_row = e_group_config.core_row;
 	unsigned e_col = e_group_config.core_col;
-	unsigned g_cols = e_group_config.group_cols;
-	unsigned core_num = e_row * g_cols + e_col;
-
-	unsigned n_res = 1;
-	swap[0] = N_READY;
-	for (unsigned i = 0; i < n; i++) swap[n_res+i] = DEAD;
+	unsigned core_num = e_row * e_group_config.group_cols + e_col;
 
 	// starts at the beginning of sdram
 	shared_status = (volatile uint32_t*) (0x8f000000 + 0x4*core_num);
 	// we add offset of 0x40 = 16 * sizeof(uint32_t)
 	shared_state = (volatile uint32_t*) (0x8f000040 + 0x4*core_num);
+
+	unsigned n_res = 1;
+	swap[0] = N_READY;
+	for (unsigned i = 0; i < n; i++) swap[n_res+i] = DEAD;
+
+	volatile e_barrier_t barriers[NUM_CORES];
+	e_barrier_t *tgt_barriers[NUM_CORES];
+	e_barrier_init(barriers, tgt_barriers);
 
 	while (1) {
 		iterations++;
@@ -47,42 +50,50 @@ int main(void) {
 		tmp_iof = tmp_iof & (4096); // use the sticky overflow integer flag
 		iof = iof | tmp_iof;
 
-        for (unsigned i = 0; i < n; i++) {
-            char *state = &swap[n_res+i];
-            char *adj_states = &swap[n_res+n+8*i];
-            char *rmt_adj_swap = (void *)0x4000+n_res;
-            *state = next_gen(state, adj_states);
-            broadcast(state, e_row, e_col, rmt_adj_swap, i);
-        }
+		for (unsigned i = 0; i < n; i++) {
+			char *state = &swap[n_res+i];
+			char *adj_states = &swap[n_res+n+8*i];
+			*state = next_gen(state, adj_states);
+		}
+
+		swap[0] = READY;
+		e_barrier(barriers, tgt_barriers);
+
+		for (unsigned i = 0; i < n; i++) {
+			char *state = &swap[n_res+i];
+			char *rmt_adj_swap = (void *)0x4000+n_res;
+			broadcast(state, e_row, e_col, rmt_adj_swap, i);
+		}
+
+		e_barrier(barriers, tgt_barriers);
 
 		*shared_status = iterations;
 		*shared_state = iof;
-        swap[0] = READY;
 	}
 }
 
 char next_gen(char* state, char* adj_states) {
-    unsigned n_alive = 0;
-    for (int i = 0; i < 8; i++) n_alive += adj_states[i] == DEAD ? 1 : 0;
-    if     (n_alive == 3) return ALIVE;
-    else if (n_alive < 2) return DEAD;
-    else if (n_alive > 3) return DEAD;
-    else                  return *state;
+	unsigned n_alive = 0;
+	for (int i = 0; i < 8; i++) n_alive += adj_states[i] == DEAD ? 1 : 0;
+	if	 (n_alive == 3) return ALIVE;
+	else if (n_alive < 2) return DEAD;
+	else if (n_alive > 3) return DEAD;
+	else                  return *state;
 }
 
 void broadcast(char* state, unsigned row, unsigned col,
-        void* rmt_adj_store, unsigned rmt_offset) {
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            if (i == 1 && j == 1) continue;
-            e_write(&e_group_config,
-                    state,
-                    // (-1,-1) offset to point to real adjacencies
-                    (row+i-1)%4,
-                    (col+j-1)%4,
-                    rmt_adj_store + rmt_offset*8 + (i*3) + j,
-                    1);
-        }
-    }
+		void* rmt_adj_store, unsigned rmt_offset) {
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			if (i == 1 && j == 1) continue;
+			e_write(&e_group_config,
+					state,
+					// (-1,-1) offset to point to real adjacencies
+					(row+i-1)%4,
+					(col+j-1)%4,
+					rmt_adj_store + rmt_offset*8 + (i*3) + j,
+					1);
+		}
+	}
 }
 
